cae473ce8a6960ce262ea711c57a0260df0afb6e
John Gaitho <jgaitho016@gmail.com>
2024-10-29 14:04:16 +0300

    complete work

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,14 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
 
+}
 
 function App() {
   return (
@@ -25,6 +32,8 @@ function App() {
       <NavBar />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
